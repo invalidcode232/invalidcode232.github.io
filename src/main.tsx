27b0bfa2e5f2fa6af348355499f31bd9549d86af
invalidcode232 <mail@invalidcode.me>
2025-05-28 12:32:11 +0800

    feat: add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+// import App from './App.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Projects from './pages/Projects';
+import Home from './pages/Home.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
