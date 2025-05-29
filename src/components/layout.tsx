58513e1f7e31edbcc5baaa24eeeae5a718f2f5a1
invalidcode232 <mail@invalidcode.me>
2025-05-29 15:51:22 +0800

    fix: update terminal screen size

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,9 +3,9 @@ import Header from "./header";
 
 export default function Layout() {
     return (
-        <div className="min-h-screen py-10 2xl:px-72 2xl:pt-20 xl:px-60 xl:pt-16 lg:px-40 md:px-32 px-8 pt-14 flex flex-col bg-slate-900">
+        <div className="min-h-screen h-screen py-10 2xl:px-72 2xl:pt-20 xl:px-60 xl:pt-16 lg:px-40 md:px-32 px-8 pt-14 flex flex-col bg-slate-900">
             <Header />
-            <main className="text-gray-200 flex-1">
+            <main className="text-gray-200 h-full flex-1">
                 <Outlet />
             </main>
         </div>
