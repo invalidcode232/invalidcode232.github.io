5b48fa964c569e0a7387aa00f6813e4808386710
invalidcode232 <mail@invalidcode.me>
2025-06-05 15:57:11 +0800

    feat: update projects page styling

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,10 +4,14 @@ import type { TFile } from "../modules/terminal-fs";
 function Projects() {
     return (
         <>
-            <h1 className="text-3xl font-semibold text-white">My Projects</h1>
+            <h1 className="text-3xl font-semibold text-white mb-2">
+                My Projects
+            </h1>
             {TFileSystem.map((file: TFile) => (
-                <div key={file.name}>
-                    <h1 className="text-2xl">{file.name.replace(".md", "")}</h1>
+                <div key={file.name} className="my-4">
+                    <h1 className="text-2xl mb-2">
+                        {file.name.replace(".md", "")}
+                    </h1>
                     <p className="text-gray-400">{file.description}</p>
                 </div>
             ))}
