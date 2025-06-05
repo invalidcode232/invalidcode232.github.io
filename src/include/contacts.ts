4ec1d35a1bfcd122b94cd9d467febd5509b45e69
invalidcode232 <mail@invalidcode.me>
2025-06-05 17:33:03 +0800

    feat: add github in contacts

--- src/include/contacts.ts
+++ src/include/contacts.ts
@@ -1,5 +1,9 @@
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+import type { IconType } from "react-icons";
+
 export interface Contact {
   name: string;
+  icon: IconType;
   text: string;
   href: string;
 }
@@ -7,14 +11,22 @@ export interface Contact {
 const CONTACTS: Contact[] = [
   {
     name: "Mail",
+    icon: FaEnvelope,
     text: "mail@invalidcode.me",
     href: "mailto:mail@invalidcode.me",
   },
   {
+    name: "Github",
+    icon: FaGithub,
+    text: "invalidcode232",
+    href: "https://github.com/invalidcode232",
+  },
+  {
     name: "Linkedin",
+    icon: FaLinkedin,
     text: "James Sungarda",
     href: "https://www.linkedin.com/in/james-sungarda/",
-  }
+  },
 ];
 
 export default CONTACTS; 
